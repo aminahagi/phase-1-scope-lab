d611fb430ec47281f6fc9bec6757898e92fb1608
aminahagi <amina.hagi@student.moringaschool>
2022-11-29 16:01:42 +0300

    complted assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toupperCase()
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'initial value'
+function changeLeastFavoriteCustomer(leastFavoriteCustomer) {
+    leastFavoriteCustomer = 'Assignment to constant variable'
+}
